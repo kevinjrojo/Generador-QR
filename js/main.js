abcf19648fbb3cc906150d253dcedc534e95500a
kevin <kevin.rojokevin17@gmail.com>
2024-06-27 08:26:14 -0300

    avanzando

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-let input = document.querySelector(".input").value;
+let input = document.querySelector(".input");
 let btn = document.querySelector(".btn1");
+let fondo = document.querySelector(".QR");
 
 btn.addEventListener("click", () => {
-  let input = document.querySelector(".input");
-  let btn = document.querySelector(".btn1");
   let logo = document.querySelector(".qrcode");
   logo.id = "logo";
   input.id = "ocultar";
   btn.id = "ocultar";
+  obtenerQr();
+  fondo.style.backgroundColor = "#4e80ee33";
+  crearBotones();
+});
+
+function obtenerQr() {
+  url = input.value;
   let qrcode = new QRCode(document.querySelector(".QR"), {
-    text: input,
-    width: 200, // Ancho del código QR
-    height: 200, // Alto del código QR
+    text: url,
     correctLevel: QRCode.CorrectLevel.H, // Nivel de corrección de errores (L, M, Q, H)
   });
-  setTimeout(function () {
-    let qrImg = document.querySelector(".QR img");
 
-    if (qrImg) {
-      qrImg.id = "miClase";
-    }
-  }, 200);
-});
+  let qrImg = document.querySelector(".QR img");
+  if (qrImg) {
+    qrImg.id = "miClase";
+  }
+}
+
+function crearBotones() {
+  let botones = document.createElement("div");
+  fondo.appendChild(botones);
+  url = input.value;
+  let descargar = document.createElement("a");
+  descargar.href = url;
+  descargar.innerText = "descargar";
+  botones.appendChild(descargar);
+  let copiar = document.createElement("button");
+  copiar.id = "btn3";
+  copiar.innerText = "copiar";
+  botones.appendChild(copiar);
+}
